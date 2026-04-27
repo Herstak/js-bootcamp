fb19fe46cc4064439d52c5b5047a8da8e603f094
Alkarim <alkarim210@gmail.com>
2026-04-27 10:08:57 +0500

    Added new static button and input with placeholder, added new function with click listen in js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let userName = prompt("Как тебя зовут");
+const myInput = document.getElementById("name-input");
+const myButton = document.getElementById("submit-btn");
+const myDiv = document.getElementById("greeting-box");
 
-let myDiv = document.getElementById("greeting");
-
-myDiv.textContent = `Привет, ${userName}!`;
+myButton.addEventListener('click', function(){
+    let name = myInput.value.trim();
+    if (name === "" || name === null) {
+        myDiv.textContent = "Анонимы здесь не выживают.";
+        myDiv.style.color = "red";
+    } else {
+        myDiv.textContent = `Привет, ${name}!`;
+        myDiv.style.color = "green";
+    }
+})
